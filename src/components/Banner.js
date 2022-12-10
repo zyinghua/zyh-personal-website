d11fdb41bb7265dd17cb61757aeb6b4c9ff8d3c3
zyinghua <yzho0149@student.monash.edu>
2022-12-10 13:29:15 +1100

    Constructed navbar, banner and some components. Currently following the youtube tutorial, components to be changed later.

---
+++ src/components/Banner.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+    // This is how we declare a state variable using Hook
+    const [loopIndex, setLoopIndex] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState("");
+    const [delta, setDelta] = useState(300 - Math.random() * 10);
+    const showPeriod = 200;
+    const deletePeriod = 1000;
+    const toRotate = [
+        "Software Engineer",
+        "Masters Student",
+        "Software Developer",
+    ];
+
+    // useEffect is kind of a once page constructed the
+    // notified effect will be in place...
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => {
+            clearInterval(ticker);
+        };
+    });
+
+    // Like in the spaceInvader project, tick() is invoked
+    // every "delta" period using setInterval().
+    const tick = () => {
+        let i = loopIndex % toRotate.length;
+        let fullText = toRotate[i];
+        let updateText = isDeleting
+            ? fullText.substring(0, text.length - 1)
+            : fullText.substring(0, text.length + 1);
+
+        setText(updateText);
+
+        if (isDeleting) {
+            setDelta((prevDelta) => prevDelta / 2);
+        }
+
+        if (!isDeleting && updateText === fullText) {
+            setIsDeleting(true);
+            setDelta(deletePeriod);
+        } else if (isDeleting && updateText === "") {
+            setIsDeleting(false);
+            setLoopIndex(loopIndex + 1);
+            setDelta(showPeriod);
+        }
+    };
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-item-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">
+                            Welcome to my Personal Website
+                        </span>
+                        <h1>{`Hi I'm Yinghua Zhou,`}</h1>
+                        <h1>
+                            {`A `}
+                            <span className="wrap">{text}</span>
+                        </h1>
+                        <p>
+                            I'm a recent Bachelor of Computer Science graduate
+                            from Monash University, and a current Master of
+                            Information Technology Student at the University of
+                            Melbourne, Specialising in AI.
+                        </p>
+                        <p>
+                            I have a strong passion in Algorithms and Data
+                            Structures as well as state-of-the-art technologies!
+                        </p>
+                        <button onClick={() => console.log("connect")}>
+                            Contact Me <ArrowRightCircle size={25} />
+                        </button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Image" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,14 @@
 import { useState, useEffect } from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import github from "../assets/img/github.svg";
+import leetcode from "../assets/img/leetcode.svg";
+import linkedIn from "../assets/img/linkedin.svg";
 
 export const NavBar = () => {
+    const linkedinLink = "https://www.linkedin.com/in/yinghua-zhou-7b95b521a/";
+    const leetcodeLink = "https://leetcode.com/nbpppp/";
+    const githubLink = "https://github.com/zyinghua";
+
     const [activeLink, setActiveLink] = useState("home");
     const [scrolled, setScrolled] = useState(false);
 
@@ -21,35 +28,85 @@ export const NavBar = () => {
         return () => window.removeEventListener("scroll", onScoll);
     }, []);
 
-    const onUpdateActiveLink = (link) => {
-        setActiveLink(link);
-    }
-
     return (
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
             <Container>
                 <Navbar.Brand href="#home">
-                    <img src={""} alt="Logo" />
+                    <span>
+                        <a className="nav-title">YINGHUA ZHOU</a>
+                    </span>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Home</Nav.Link>
-                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Home</Nav.Link>
+                        <Nav.Link
+                            href="#home"
+                            className={
+                                activeLink === "home"
+                                    ? "active navbar-link"
+                                    : "navbar-link"
+                            }
+                            onClick={() => setActiveLink("home")}
+                        >
+                            Home
+                        </Nav.Link>
+                        <Nav.Link
+                            href="#skills"
+                            className={
+                                activeLink === "skills"
+                                    ? "active navbar-link"
+                                    : "navbar-link"
+                            }
+                            onClick={() => setActiveLink("skills")}
+                        >
+                            Skills
+                        </Nav.Link>
+                        <Nav.Link
+                            href="#blogs"
+                            className={
+                                activeLink === "blogs"
+                                    ? "active navbar-link"
+                                    : "navbar-link"
+                            }
+                            onClick={() => setActiveLink("blogs")}
+                        >
+                            Blogs
+                        </Nav.Link>
+                        <Nav.Link
+                            href="#projects"
+                            className={
+                                activeLink === "projects"
+                                    ? "active navbar-link"
+                                    : "navbar-link"
+                            }
+                            onClick={() => setActiveLink("projects")}
+                        >
+                            Projects
+                        </Nav.Link>
                     </Nav>
                     <span className="navbar-text">
                         <div className="social-icon">
-                            <a href="#">
-                                <img src={} alt="" />
+                            <a href={githubLink}>
+                                <img
+                                    src={github}
+                                    alt="Link to Yinghua's Github site"
+                                    className="black-svg-icon"
+                                />
                             </a>
-                            <a href="#">
-                                <img src={} alt="" />
+                            <a href={leetcodeLink}>
+                                <img
+                                    src={leetcode}
+                                    alt="Link to Yinghua's Leetcode site"
+                                    className="black-svg-icon"
+                                />
                             </a>
-                            <a href="#">
-                                <img src={} alt="" />
+                            <a href={linkedinLink}>
+                                <img
+                                    src={linkedIn}
+                                    alt="Link to Yinghua's Linkedin"
+                                />
                             </a>
                         </div>
                         <button
