468e776dd8f78841ad563ad84192d11f4ac890bf
zyinghua <yzho0149@student.monash.edu>
2022-12-21 14:49:01 +1100

    Added skill section

---
+++ src/components/Projects.js
@@ -0,0 +1,23 @@
+
+
+export const Projects = () => {
+    const projects = [
+        {
+            title: "Food Comparison Android App",
+            description: "Front-end Android Development of the App",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Food Comparison Android App",
+            description: "Front-end Android Development of the App",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Food Comparison Android App",
+            description: "Front-end Android Development of the App",
+            imgUrl: projImg1,
+        },
+    ];
+
+    return ();
+};
