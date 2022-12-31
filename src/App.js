b9e1bbb99de7ff675997648fe59260440a4dedf0
zyinghua <yzho0149@student.monash.edu>
2023-01-01 00:16:48 +1100

    Added education section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
+import { Education } from "./components/Education";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
@@ -13,6 +14,7 @@ function App() {
             <NavBar />
             <Banner />
             <Skills />
+            <Education />
             <Projects />
             <Contact />
             <Footer />
---
+++ src/components/Education.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+export const Education = () => {
+    return (
+        <section className="education" id="education">
+            <Container>
+                <Row>
+                    <Col>
+                        <h2>Education</h2>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
