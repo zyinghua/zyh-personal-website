63784d06324c02fddef874793e068fdc71be7e6f
zyinghua <yzho0149@student.monash.edu>
2022-12-31 21:19:33 +1100

    Added projectCard view more feature, with scannera proj added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,26 @@
 import { Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, viewMoreUrl }) => {
     return (
         <Col sm={6} md={4}>
             <div className="proj-imgbox">
                 <img src={imgUrl} />
-                <div className="proj-txtx">
+                <div className="proj-txt">
                     <h4>{title}</h4>
                     <span>{description}</span>
+                    <a
+                        target="_blank"
+                        href={viewMoreUrl}
+                        style={{
+                            "text-decoration": "none",
+                        }}
+                    >
+                        <div className="proj-view-more-txt">
+                            View More
+                            <ArrowRightCircle size={19} />
+                        </div>
+                    </a>
                 </div>
             </div>
         </Col>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,14 +2,17 @@ import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import "react-multi-carousel/lib/styles.css";
 import scannera_cover from "../assets/img/scannera-proj-cover.jpg";
+import scannera_vm_url from "../assets/Scannera_Project_Doc.pdf";
 
 export const Projects = () => {
     const projects = [
         {
             title: "Android App - Scannera",
             description:
-                "A better way to compare food for retail store customers. [#Barcode-Scanning, #Android, #Flask, #SQL, #OCR, #Java, #Python, #Hybrid - Agile&Waterfall]",
+                "A better way to compare food for retail store customers.\
+                 [#Barcode-Scanning, #Android, #Flask, #SQL, #OCR, #Java, #Python, #Hybrid - Agile&Waterfall]",
             imgUrl: scannera_cover,
+            viewMoreUrl: scannera_vm_url,
         },
     ];
 
