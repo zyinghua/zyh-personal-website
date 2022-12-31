1b9c512b6e89ba4115652c126e9cc0dcc07ab1b2
zyinghua <yzho0149@student.monash.edu>
2022-12-31 18:22:34 +1100

    Projects section initial stage ready with scannera embedded

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,34 +1,17 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import projImg1 from "../assets/img/project-img1.png";
+import scannera_cover from "../assets/img/scannera-proj-cover.jpg";
 
 export const Projects = () => {
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5,
+    const projects = [
+        {
+            title: "Android App - Scannera",
+            description:
+                "A better way to compare food for retail store customers. [#Barcode-Scanning, #Android, #Flask, #SQL, #OCR, #Java, #Python, #Hybrid - Agile&Waterfall]",
+            imgUrl: scannera_cover,
         },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-        },
-    };
-
-    const project = {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg1,
-    };
+    ];
 
     return (
         <section className="projects" id="projects">
@@ -37,15 +20,13 @@ export const Projects = () => {
                     <Row>
                         <Col>
                             <h2>Projects</h2>
-                            <Carousel
-                                responsive={responsive}
-                                infinite={true}
-                                className="owl-carousel owl-theme project-slider"
-                            >
-                                <div className="item">
-                                    <ProjectCard key="1" {...project} />
-                                </div>
-                            </Carousel>
+                            <Row>
+                                {projects.map((project, index) => {
+                                    return (
+                                        <ProjectCard key={index} {...project} />
+                                    );
+                                })}
+                            </Row>
                         </Col>
                     </Row>
                 </Container>
