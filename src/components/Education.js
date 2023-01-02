40dbcef1a7f423e031ff988348b9d035c2beee30
zyinghua <yzho0149@student.monash.edu>
2023-01-02 16:16:36 +1100

    education instance clickable with window visible and close button avaialble

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,6 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { BiCalendar } from "react-icons/bi";
+import { BiCalendar, BiX } from "react-icons/bi";
 
 export const Education = () => {
     const education_data = [
@@ -9,14 +9,20 @@ export const Education = () => {
             institute: "The University of Melbourne",
             start: "2022",
             end: "Present",
+            wam: "-",
+            desc: "1111",
         },
         {
             degree: "Bachelor of Computer Science",
             institute: "Monash University",
             start: "2020",
             end: "2022",
+            wam: "84+",
+            desc: "Good good study",
         },
     ];
+
+    const [activeModal, setActiveModal] = useState(null);
 
     return (
         <section className="education" id="education">
@@ -26,10 +32,14 @@ export const Education = () => {
                         <h2>Education</h2>
                         <div className="education-content">
                             {education_data.map((instance, index) => {
-                                if (index % 2 == 0) {
+                                if (index % 2 === 0) {
                                     return (
                                         <div className="education-data">
-                                            <div>
+                                            <div
+                                                onClick={() =>
+                                                    setActiveModal(index)
+                                                }
+                                            >
                                                 <h3 className="education-degree">
                                                     {instance.degree}
                                                 </h3>
@@ -47,6 +57,33 @@ export const Education = () => {
                                                 <span className="education-rounder" />
                                                 <span className="education-line" />
                                             </div>
+
+                                            <div
+                                                className={
+                                                    activeModal === index
+                                                        ? "education-modal education-modal-active"
+                                                        : "education-modal"
+                                                }
+                                            >
+                                                <div className="education-modal-content">
+                                                    <BiX
+                                                        className="education-modal-close"
+                                                        onClick={() =>
+                                                            setActiveModal(null)
+                                                        }
+                                                    />
+                                                    <h3 className="education-modal-degree">
+                                                        {instance.degree}
+                                                    </h3>
+                                                    <p className="education-modal-institute">
+                                                        {instance.institute}
+                                                    </p>
+                                                    <p>WAM: {instance.wam}</p>
+                                                    <p className="education-modal-desc">
+                                                        {instance.desc}
+                                                    </p>
+                                                </div>
+                                            </div>
                                         </div>
                                     );
                                 } else {
@@ -58,7 +95,11 @@ export const Education = () => {
                                                 <span className="education-line" />
                                             </div>
 
-                                            <div>
+                                            <div
+                                                onClick={() =>
+                                                    setActiveModal(index)
+                                                }
+                                            >
                                                 <h3 className="education-degree">
                                                     {instance.degree}
                                                 </h3>
@@ -69,6 +110,35 @@ export const Education = () => {
                                                     <BiCalendar />
                                                     {instance.start} -{" "}
                                                     {instance.end}
+                                                </div>
+                                            </div>
+
+                                            <div
+                                                className={
+                                                    activeModal === index
+                                                        ? "education-modal education-modal-active"
+                                                        : "education-modal"
+                                                }
+                                            >
+                                                <div className="education-modal-content">
+                                                    <BiX
+                                                        className="education-modal-close"
+                                                        onClick={() => {
+                                                            setActiveModal(
+                                                                null
+                                                            );
+                                                        }}
+                                                    />
+                                                    <h3 className="education-modal-degree">
+                                                        {instance.degree}
+                                                    </h3>
+                                                    <p className="education-modal-institute">
+                                                        {instance.institute}
+                                                    </p>
+                                                    <p>WAM: {instance.wam}</p>
+                                                    <p className="education-modal-desc">
+                                                        {instance.desc}
+                                                    </p>
                                                 </div>
                                             </div>
                                         </div>
