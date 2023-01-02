10b1b4bf55f720df910342d265d3ccf8e3ef7176
zyinghua <yzho0149@student.monash.edu>
2023-01-02 13:03:17 +1100

    Optimised Education.js from hardcoding to variable list.map

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,56 +3,79 @@ import { Container, Row, Col } from "react-bootstrap";
 import { BiCalendar } from "react-icons/bi";
 
 export const Education = () => {
+    const education_data = [
+        {
+            degree: "Master of Information Technology (With specialisation in AI)",
+            institute: "The University of Melbourne",
+            start: "2022",
+            end: "Present",
+        },
+        {
+            degree: "Bachelor of Computer Science",
+            institute: "Monash University",
+            start: "2020",
+            end: "2022",
+        },
+    ];
+
     return (
         <section className="education" id="education">
             <Container>
                 <Row className="align-item-center">
                     <Col>
                         <h2>Education</h2>
-
                         <div className="education-container">
                             <div className="education-content">
-                                <div className="education-data">
-                                    <div>
-                                        <h3 className="education-degree">
-                                            Master of Information Technology
-                                            (With specialisation in AI)
-                                        </h3>
-                                        <span className="education-institute">
-                                            The University of Melbourne
-                                        </span>
-                                        <div className="education-calendar">
-                                            <BiCalendar />
-                                            2022 - Present
-                                        </div>
-                                    </div>
+                                {education_data.map((instance, index) => {
+                                    if (index % 2 == 0) {
+                                        return (
+                                            <div className="education-data">
+                                                <div>
+                                                    <h3 className="education-degree">
+                                                        {instance.degree}
+                                                    </h3>
+                                                    <span className="education-institute">
+                                                        {instance.institute}
+                                                    </span>
+                                                    <div className="education-calendar">
+                                                        <BiCalendar />
+                                                        {instance.start} -{" "}
+                                                        {instance.end}
+                                                    </div>
+                                                </div>
 
-                                    <div>
-                                        <span className="education-rounder" />
-                                        <span className="education-line" />
-                                    </div>
-                                </div>
+                                                <div>
+                                                    <span className="education-rounder" />
+                                                    <span className="education-line" />
+                                                </div>
+                                            </div>
+                                        );
+                                    } else {
+                                        return (
+                                            <div className="education-data">
+                                                <div />
+                                                <div>
+                                                    <span className="education-rounder" />
+                                                    <span className="education-line" />
+                                                </div>
 
-                                <div className="education-data">
-                                    <div />
-                                    <div>
-                                        <span className="education-rounder" />
-                                        <span className="education-line" />
-                                    </div>
-
-                                    <div>
-                                        <h3 className="education-degree">
-                                            Bachelor of Computer Science
-                                        </h3>
-                                        <span className="education-institute">
-                                            Monash University
-                                        </span>
-                                        <div className="education-calendar">
-                                            <BiCalendar />
-                                            2020 - 2022
-                                        </div>
-                                    </div>
-                                </div>
+                                                <div>
+                                                    <h3 className="education-degree">
+                                                        {instance.degree}
+                                                    </h3>
+                                                    <span className="education-institute">
+                                                        {instance.institute}
+                                                    </span>
+                                                    <div className="education-calendar">
+                                                        <BiCalendar />
+                                                        {instance.start} -{" "}
+                                                        {instance.end}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    }
+                                })}
                             </div>
                         </div>
                     </Col>
