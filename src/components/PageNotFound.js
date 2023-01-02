31b54e66ec41a7d300e55397df8eca0ee3a7eafe
zyinghua <yzho0149@student.monash.edu>
2023-01-02 23:27:40 +1100

    Updated page not found page, with a return back to home page button

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,19 +1,24 @@
 import React from "react";
 import pnf from "../assets/img/404-page-not-found.gif";
+import { Footer } from "../components/Footer";
+import { Link } from "react-router-dom";
 
 export const PageNotFound = () => {
     return (
-        <div className="page-not-found" id="page-not-found">
-            <h2>404 Page Not Found</h2>
-            <img
-                src={pnf}
-                alt="404-Page-Not-Found.gif"
-                className="page-not-found-img"
-            />
-            <p>
-                The page you are trying to access is not found, please make sure
-                your URL is correct.
-            </p>
+        <div>
+            <div className="page-not-found" id="page-not-found">
+                <h2>404 Page Not Found</h2>
+                <img
+                    src={pnf}
+                    alt="404-Page-Not-Found.gif"
+                    className="page-not-found-img"
+                />
+                <p>
+                    The page you are trying to access is not found, please make
+                    sure your URL is correct. <br />
+                    To return back to home page, <Link to="/">click here</Link>.
+                </p>
+            </div>
         </div>
     );
 };
