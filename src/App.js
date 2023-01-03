b60c703cd11f2f16a4a2b949dc46912e82f56914
zyinghua <yzho0149@student.monash.edu>
2023-01-03 17:19:45 +1100

    added basename for the router

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename="/">
             <Routes>
                 <Route
                     exact
