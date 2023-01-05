240979783b9de345a3e49f10433ea67bae8bd9e5
zyinghua <yzho0149@student.monash.edu>
2023-01-05 14:22:48 +1100

    Added styling for code in react markdown page

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,8 @@ import { React, useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import md from "../assets/posts/leetcode-142-linked-list-cycle-2.md";
 
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+
 export const Post = () => {
     const [mdFile, setMdFile] = useState("");
 
@@ -13,9 +15,27 @@ export const Post = () => {
 
     return (
         <div className="mark-down-render">
-            <h4>Title</h4>
+            <h4>Title:</h4>
             <h4>Posted on:</h4>
-            <ReactMarkdown children={mdFile} />
+            <ReactMarkdown
+                children={mdFile}
+                components={{
+                    code({ node, inline, className, children, ...props }) {
+                        const match = /language-(\w+)/.exec(className || "");
+                        return !inline && match ? (
+                            <SyntaxHighlighter
+                                children={String(children).replace(/\n$/, "")}
+                                language={match[1]}
+                                {...props}
+                            />
+                        ) : (
+                            <code className={className} {...props}>
+                                {children}
+                            </code>
+                        );
+                    },
+                }}
+            />
         </div>
     );
 };
