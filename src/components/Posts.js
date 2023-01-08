8f3420333a3bdc2d43519eb875cff87490bf5e84
zyinghua <yzho0149@student.monash.edu>
2023-01-08 15:45:49 +1100

    added a note indicating markdown cant be found error on posts.js for temporary notice

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,6 +6,10 @@ export const Posts = () => {
     return (
         <div className="posts" id="posts">
             <h2>Latest Posts 📰</h2>
+            <p style={{ fontSize: "24px", color: "red", textAlign: "center" }}>
+                Currently experiencing markdown files unable to be found on
+                github pages... Coming back soon.
+            </p>
             {posts.map((post, index) => {
                 return <PostCard key={index} {...post} />;
             })}
